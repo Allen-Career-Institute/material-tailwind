6650f05bebc59433a330c73f670a7eb469a67020
Dinesh Kumar D <dinesh.kumar4@allen.in>
2024-12-01 17:50:02 +0530

    chore: enabled optimization on calculation of merge

--- packages/material-tailwind-react/src/context/theme.js
+++ packages/material-tailwind-react/src/context/theme.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useMemo } from "react";
 import PropTypes from "prop-types";
 import merge from "deepmerge";
 import theme from "../theme/index";
@@ -9,7 +9,7 @@ const MaterialTailwindTheme = createContext(theme);
 MaterialTailwindTheme.displayName = "MaterialTailwindThemeProvider";
 
 function ThemeProvider({ value = theme, children }) {
-  const mergedValue = merge(theme, value, { arrayMerge: combineMerge });
+  const mergedValue = useMemo(() => merge(theme, value, { arrayMerge: combineMerge }), [value]);
 
   return (
     <MaterialTailwindTheme.Provider value={mergedValue}>{children}</MaterialTailwindTheme.Provider>
